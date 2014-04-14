8786516e1b9db02d9555d32b58ac39a42065d800
Gene Cumm <gene.cumm@gmail.com>
2014-04-14 13:50:53 -0400

    log2txt.pl: Make data corrections
    Was creating erroneous data elements due to bad regex; more test code.
    
    Signed-off-by: Gene Cumm <gene.cumm@gmail.com>

--- log2txt.pl
+++ log2txt.pl
@@ -1,5 +1,14 @@
 #!/usr/bin/perl
 
+# log2txt.pl
+# Copyright 2014, Gene Cumm <genecumm@gmail.com>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+# Boston MA 02110-1301, USA; either version 2 of the License, or
+# (at your option) any later version; incorporated herein by reference.
+
 # Parse Sophos UTM v9/Astaro ASG v8 log lines to text formats
 
 # This list is based on the fields in http.log from UTM-9.2 for pass/block plus error
@@ -22,11 +31,11 @@ sub syslogmsg2hash {
 	my ($ins) = @_;
 	my %d, $msg, @s, $e, $f, $v;
 
-	@s = split(/([-a-z]+="(?:.*?[^\\]|)" ?)/, $ins);
+	@s = split(/([-a-z]+="(?:|.*?[^\\])") ?/, $ins);
 	$msg = $s[scalar(@s)-1];	# will contain \n
 	foreach $e (@s[0..(scalar(@s) - 2)]) {
-		if (!($e eq '')) {
-print '-', $e, "-\n";
+		if (!(($e eq '') or ($e eq ' '))) {
+# print '-', $e, "-\n";
 			($f, $v) = split(/=/, $e, 2);
 			$d{$f} = $v;
 		}
@@ -75,6 +84,11 @@ sub sysloghash2csv {
 	}
 # print join(';', keys %d), "\n";
 # print join('_', @$k), "\n";
+	foreach $f (keys %d) {
+		push @$k, $f;
+		$r .= ',' . $d{$f};
+		delete $d{$f};
+	}
 	$r .=  "\n";	# msg had \n chomp()'d
 	return $r;
 }
@@ -86,15 +100,16 @@ sub log2txt_test {
 # 	}
 
 	# test syslog2hash
-	my %d;
+	my %d, $s;
 	my @k = ('ts', 'host', 'proc', 'id', 'severity', 'sys', 'sub', 'name', 'action', 'method', 'srcip', 'dstip', 'user', 'statuscode', 'cached', 'profile', 'filteraction', 'size', 'request', 'url', 'exceptions', 'error', 'authtime', 'dnstime', 'cattime', 'avscantime', 'fullreqtime', 'device', 'auth', 'reason', 'category', 'reputation', 'categoryname', 'content-type', 'function', 'file', 'line', 'message', 'msg');
 	while (<STDIN>) {
 		%d = syslog2hash($_);
 		# print ($d{'ts'}, ',', $d{'host'}, ',', $d{'proc'} , "\n");
 		# now test sysloghash2csv
-		print sysloghash2csv(\%d, \@k);
+		# print sysloghash2csv(\%d, \@k);
+		$s = sysloghash2csv(\%d, \@k);
 	}
-	print join(',', @k), "\n";
+	print "FinalKeys\n" , join(',', @k), "\n";
 }
 
 sub log2txt_main {
